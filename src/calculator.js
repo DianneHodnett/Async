c43102974462f529b57edd02e85254bbca3d3496
Dianne <diannehodnett@gmail.com>
2018-04-07 13:32:23 -0600

    first commit

---
+++ src/calculator.js
@@ -0,0 +1,32 @@
+/**
+ * Create a variadic function (function that accepts a variable amount of values) and uses
+ * reduce function to sum the values and return the reduced value as a promise.
+ *
+ * Use only async/await
+ *
+ * @param {Array<string>} values
+ * @returns {Promise<Number>}
+ */
+const add = async (...values) => {
+
+}
+
+/**
+ * Create a variadic function (function that accepts a variable amount of values) and uses
+ * reduce function to subtract values and return the reduced value as a promise.
+ *
+ * Use only async/await
+ *
+ * @param {Array<string>} values
+ * @returns {Promise<Number>}
+ */
+const subtract = async (...values) => {
+
+}
+
+/**
+ * Export out the add, and subtract
+ */
+export default {
+
+}
